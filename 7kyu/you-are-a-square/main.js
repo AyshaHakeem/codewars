561f9dbb87c0aac932b949ff950b271d51f50fe1
Aysha <ayshapp468@gmail.com>
2022-07-21 19:57:32 +0530

    Codewars solution to 'You are a square'

---
+++ 7kyu/you-are-a-square/main.js
@@ -0,0 +1,11 @@
+/*
+Description:
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+*/
+
+// My solution
+let isSquare = n => Number.isInteger( Math.sqrt(n))
