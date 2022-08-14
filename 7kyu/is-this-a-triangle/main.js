4f029a9482761dd08d3a2f762bb49ab95af63187
Aysha <ayshapp468@gmail.com>
2022-08-14 22:10:55 +0530

    Codewars solution to 'Is this a triangle?'

---
+++ 7kyu/is-this-a-triangle/main.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+*/
+//My Solution:
+function isTriangle(a,b,c){
+    return (a+b)>c && (b+c)>a && (c+a)>b
+}
