f84d45d27d871d1b9f6d194c8dcf63748fe1a7ef
Aysha <ayshapp468@gmail.com>
2022-09-26 22:19:27 +0530

    Codewars solution to 'Ones and Zeroes'

---
+++ 7kyu/ones-and-zeroes/main.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+*/
+
+// My Solution
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(''),2)
+  };
