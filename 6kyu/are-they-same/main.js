1700e1e748b432893c3125811feac9334c87478e
Aysha <ayshapp468@gmail.com>
2022-12-18 17:35:40 +0530

    Codewars solution to 'Are they the same'

---
+++ 6kyu/are-they-same/main.js
@@ -0,0 +1,58 @@
+/*
+DESCRIPTION:
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+*/
+
+// My Solution
+
+// Refactored
+
+function comp(array1, array2){
+    let arr1 = array1.sort((a,b)=>a-b).map(x=>x*x)
+    let arr2 = array2.sort((a,b)=>a-b)
+    return arr1.every((n,i)=> n==arr2[i])      
+}
+
+//first solution
+
+function comp(array1, array2){
+    let result = 0;
+    if(array1 && array2) {
+        let sqr = 0;
+        let arr1 = array1.sort((a,b)=>a-b),
+            arr2 = array2.sort((a,b)=>a-b)
+        arr1.forEach((n,i)=>{
+            sqr = Math.pow(n,2)
+            if(!(arr2[i]==sqr)) result++
+        })
+    }
+    else{
+        result++
+    }
+    return result ? false : true
+}
+
