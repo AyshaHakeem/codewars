baac617b02daaa590a09c557780a24734959712d
Aysha <ayshapp468@gmail.com>
2023-04-09 22:48:52 +0530

    Codewars solution to 'Product of consecutive Fibonocci numbers'

---
+++ 5kyu/product-of-fib-nums/main.js
@@ -0,0 +1,71 @@
+/*
+DESCRIPTION:
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return:
+(depend on the language)
+
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true], 
+productFib(800) # should return [34, 55, false], 
+-----
+productFib(714) # should return {21, 34, 1}, 
+productFib(800) # should return {34, 55, 0},        
+-----
+productFib(714) # should return {21, 34, true}, 
+productFib(800) # should return {34, 55, false}, 
+*/
+
+// My Solution
+// second
+function productFib(prod){
+    let [x,y] = [0,1]
+    while( (x*y)<prod ) { [x,y] = [y , x+y] }
+    return [x,y, (x*y)==prod]
+}
+
+// first
+function productFib(prod){
+    let fib = [0,1],
+        num = 0 ,
+        result = [],
+        i = 2;
+    while(num<prod){
+        fib[i] = (fib[i-1]+fib[i-2])
+        num = fib[i-1]*fib[i] 
+        i++
+    }
+    result = [ fib[i-2], fib[i-1] ]
+    return (num==prod) ? [...result,true] : [...result,false]
+}
+
+
+
+
+
+
