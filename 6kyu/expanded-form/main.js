02ad49f6a62b8d6d1e18e10fa2405474f45c0245
Aysha <ayshapp468@gmail.com>
2024-04-08 10:00:00 +0530

    Codewars solution to 'Write Number in Expanded Form'

---
+++ 6kyu/expanded-form/main.js
@@ -0,0 +1,21 @@
+/*
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+function expandedForm(num) {
+  let str = String(num),
+    result = [],
+    multiple;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "0") {
+      multiple = Number("1".padEnd(str.length - i, 0));
+      result.push(Number(str[i]) * multiple);
+    }
+  }
+  return result.join(" + ");
+}
