640c90e2b02c1da6f6d9bfb3c87d3adc6abc563d
Aysha <ayshapp468@gmail.com>
2024-04-21 10:00:00 +0530

    Neetcode solution to 'Anagram Groups'

---
+++ neetcode/anagram-groups.js
@@ -0,0 +1,41 @@
+/*
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+Example 2:
+
+Input: strs = ["x"]
+
+Output: [["x"]]
+Example 3:
+
+Input: strs = [""]
+
+Output: [[""]]
+Constraints:
+
+1 <= strs.length <= 1000.
+0 <= strs[i].length <= 100
+strs[i] is made up of lowercase English letters
+*/
+
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @return {string[][]}
+   */
+  groupAnagrams(strs) {
+    let obj = {};
+    strs.forEach((x) => {
+      let str = x.split("").sort().join("");
+      obj.hasOwnProperty(str) ? obj[str].push(x) : (obj[str] = [x]);
+    });
+    return Object.values(obj);
+  }
+}
