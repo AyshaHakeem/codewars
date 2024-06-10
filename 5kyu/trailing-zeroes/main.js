8c1cebcee92faa380d6e87a7e69acc3771c889a6
Aysha <ayshapp468@gmail.com>
2024-06-10 10:01:30 +0530

    solution to 'Number of trailing zeros of N!'

---
+++ 5kyu/trailing-zeroes/main.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 *  ... * N
+
+Be careful 1000! has 2568 digits...
+
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+Examples
+N	Product	N factorial	Trailing zeros
+6	1*2*3*4*5*6	720	1
+12	1*2*3*4*5*6*7*8*9*10*11*12	479001600	2
+Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+*/
+function zeros(n) {
+  let count = 0;
+  while (n >= 5) {
+    n = Math.floor(n / 5);
+    count += n;
+  }
+  return count;
+}
+
+//  n:30 n:6
+//  n>5  n>5
+//  6    1
