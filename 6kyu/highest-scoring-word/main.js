063779e6ab62ec33031af6c0b79162f5ff9163c2
Aysha <ayshapp468@gmail.com>
2024-06-14 10:00:30 +0530

    Solution to 'Highest Scoring Word'

---
+++ 6kyu/highest-scoring-word/main.js
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+
+function high(x) {
+  const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']    
+  let words = x.split(" ");
+  let scores = words.map((x) =>
+    x.split("").reduce((sum, l) => (sum += alphabets.indexOf(l) + 1), 0)
+  );
+  return words[scores.indexOf(Math.max(...scores))];
+}
